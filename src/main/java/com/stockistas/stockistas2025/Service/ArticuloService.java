4cafca001eb7952d9c0ef9de5feb7c0f1115c935
Matias Roiz <roizmatias10@gmail.com>
2025-06-06 15:34:37 -0300

    cambios

--- src/main/java/com/stockistas/stockistas2025/Service/ArticuloService.java
+++ src/main/java/com/stockistas/stockistas2025/Service/ArticuloService.java
@@ -151,6 +151,7 @@ public class ArticuloService {
     public List<Articulo> obtenerArticulosCriticos() {
         List<Articulo> todos = articuloRepository.findAll();
         return todos.stream()
+                .filter(a -> a.getStockSeguridad() !=null)
                 .filter(a -> a.getFechaHoraBajaArticulo() == null) // activo
                 .filter(a -> a.getStockActual() <= a.getStockSeguridad())
                 .toList();
